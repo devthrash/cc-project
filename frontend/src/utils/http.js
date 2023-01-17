df1fa78c48e1d61ed326e08355460d00dedb0300
Laurentiu-Cristian Danci <72353513+devthrash@users.noreply.github.com>
2023-01-18 00:52:06 +0200

    move improvs

--- frontend/src/utils/http.js
+++ frontend/src/utils/http.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:9999'
+    baseURL: import.meta.env.VITE_BACKEND_HOST
+    // baseURL: 'http://localhost:9999'
 });
